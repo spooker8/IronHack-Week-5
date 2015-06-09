1186d58c7573c9dad01faada462e92db89d5fa23
Anand Kumar <spooker8@yahoo.com>
2015-06-09 09:04:42 +0200

    Code has errors.

---
+++ gcd-learning/gcd-learning/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gcd-learning
+//
+//  Created by Anand Kumar on 5/31/15.
+//  Copyright (c) 2015 anand. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
